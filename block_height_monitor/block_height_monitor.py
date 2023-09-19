6ba499babb860cb5eea828abf9c82909d97b35ec
spuddyminer <sloetterle@tellor.io>
2023-09-19 14:20:39 -0400

    added testing things

--- block_height_monitor/block_height_monitor.py
+++ block_height_monitor/block_height_monitor.py
@@ -1,14 +1,16 @@
-import time
+import logging
 import os
-import emoji
-from web3 import Web3
+import time
+
 from discordwebhook import Discord
 from dotenv import load_dotenv
-import logging
+from web3 import Web3
 
 load_dotenv()
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', filename='logs.log', filemode='a')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", filename="logs.log", filemode="a"
+)
 
 # Create a logger object
 logger = logging.getLogger("block_height_monitor")
@@ -31,56 +33,65 @@ console_handler.setFormatter(formatter)
 
 # Add the handlers to the logger
 # add the handler to the root logger
-logging.getLogger('').addHandler(console_handler)
+logging.getLogger("").addHandler(console_handler)
 logger.addHandler(file_handler)
 logger.addHandler(console_handler)
 
-#read variables from .env
+# read variables from .env
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 PRIMARY_NODE_ENDPOINT = os.getenv("PRIMARY_NODE_ENDPOINT")
 SECONDARY_NODE_ENDPOINT = os.getenv("SECONDARY_NODE_ENDPOINT")
 INTERVAL = int(os.getenv("INTERVAL"))
 
-#set variables
+# set variables
 alert_bot = Discord(url=DISCORD_WEBHOOK_URL)
 primary = Web3(Web3.HTTPProvider(PRIMARY_NODE_ENDPOINT))
 secondary = Web3(Web3.HTTPProvider(SECONDARY_NODE_ENDPOINT))
 alarms = 0
 
-def get_primary_block(primary):
+
+def get_primary_block(primary: int) -> int:
     try:
         primary_block_number = primary.eth.block_number
         logging.info(f"Primary node highest block: {primary_block_number}")
+        return primary_block_number
     except Exception:
         return 1
 
-def get_secondary_block(secondary):
+
+def get_secondary_block(secondary: int) -> int:
     try:
         secondary_block_number = secondary.eth.block_number
         logging.info(f"Secondary node highest block: {secondary_block_number}")
+        return secondary_block_number
     except Exception:
         return 2
-    
-while True:
-    if get_primary_block(primary) == get_secondary_block(secondary):
-        logging.info("Node is all good. \U0001F60E")
-        time.sleep(INTERVAL)
-        alarms = 0
-    elif get_primary_block(primary) == 1:
-        logging.info("Error getting the block number. Trying again in 60s...")
-        alert_bot.post(content="Primary node could not be reached.")
-        time.sleep(60)
-    elif get_secondary_block(secondary) == 2:
-        logging.info("Secondary node cannot be reached. Trying again in 60s...")
-        alert_bot.post(content="Secondary node could not be reached.")
-        time.sleep(60)
-    else:
-        alert_bot.post(content="\U0001F6A8 NODE IS OUT OF SYNC! \U0001F6A8")
-        logging.info("NODES MAY BE OUT OF SYNC! \U0001F6A8 ALERT SENT!")
-        alarms += 1
-        if alarms > 5:
-            logging.info("node is drunk send shutdown signal")
-            #alert_bot.post(content="Node shutdown signal sent. Switch to backup nodes now!")
-            time.sleep(3600)
-        else:
+
+
+def main():
+    while True:
+        if get_primary_block(primary) == get_secondary_block(secondary):
+            alarms = 0
+            logging.info("Node is all good. \U0001F60E")
+            time.sleep(INTERVAL)
+        elif get_primary_block(primary) == 1:
+            logging.info("Primary node could not be reached. Trying again in 60s...")
+            alert_bot.post(content="Primary node could not be reached.")
             time.sleep(60)
+        elif get_secondary_block(secondary) == 2:
+            logging.info("Secondary node could not be reached. Trying again in 60s...")
+            alert_bot.post(content="Secondary node could not be reached.")
+            time.sleep(60)
+        else:
+            alarms += 1
+            alert_bot.post(content="\U0001F6A8 NODE IS OUT OF SYNC! \U0001F6A8")
+            logging.info("NODES MAY BE OUT OF SYNC! \U0001F6A8 ALERT SENT!")
+            if alarms > 5:
+                logging.info("node is drunk send shutdown signal")
+                # alert_bot.post(content="Node shutdown signal sent. Switch to backup nodes now!")
+                time.sleep(3600)
+            else:
+                time.sleep(60)
+
+
+#block_height_monitor()
--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-from setuptools import setup, find_packages
-
+from setuptools import find_packages
+from setuptools import setup
 
 def read_requirements():
-    with open('requirements.txt', 'r') as req:
+    with open("requirements.txt", "r") as req:
         content = req.read()
-        requirements = content.split('\n')
+        requirements = content.split("\n")
 
     return requirements
 
 
 setup(
     name='block-height-monitor',
-    version='0.1.0',
+    version='0.1',
     packages=find_packages(),
     install_requires=read_requirements(),
+    entry_points={
+        'console_scripts': [
+            'monitor=block_height_monitor.block_height_monitor:main'
+            ],
+    },
 )
